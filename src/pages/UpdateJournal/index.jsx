ea78d57c85d91a8692f46f32eb6be7e435ecdc4a
Lee Yujin <yuwls1102@gmail.com>
2025-07-01 01:41:34 +0900

    feat:ExerciseLog API 연동

--- src/pages/UpdateJournal/index.jsx
+++ src/pages/UpdateJournal/index.jsx
@@ -2,6 +2,7 @@ import styles from "./UpdateJournal.module.css";
 import { Header } from "../../components/commonComponents/Header/index.jsx";
 import { UpdateJournalContent } from "../../components/pagesComponents/journalsComponents/UpdateJournalContent/index.jsx";
 import { useParams } from "react-router-dom";
+
 export const UpdateJournal = () => {
   const { journalId } = useParams();
   return (
